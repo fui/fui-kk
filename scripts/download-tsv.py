98c92314eb9c8b0ad469ffd476277f224b0684f4
Erik Vesteraas <erik@vestera.as>
2016-06-04 14:27:13 +0200

    Make download-tsv more flexible

--- scripts/download-tsv.py
+++ scripts/download-tsv.py
@@ -1,4 +1,4 @@
-# Downloads TSV files for all of a users forms
+# Downloads TSV and HTML reports for a user from nettskjema.uio.no
 
 # Dependencies:
 # pip install selenium
@@ -14,22 +14,40 @@
 import getpass
 import requests
 import os
+import sys
+import argparse
 
 from selenium import webdriver
+
+argparser = argparse.ArgumentParser(description='Download report data from nettskjema.uio.no')
+argparser.add_argument('--out', '-o', help='Output directory (default=".")', type=str, default='.')
+argparser.add_argument('--filter', '-f', help='String to filter by', type=str)
+argparser.add_argument('--username', '-u', help='Username for login', type=str)
+argparser.add_argument('--password', '-p', help='Password for login', type=str)
+argparser.add_argument('--tsv', help='Download TSV files', action="store_true")
+argparser.add_argument('--html', help='Download HTML reports', action="store_true")
+args = argparser.parse_args()
+
+if not args.tsv and not args.html:
+    args.tsv = True
+    args.html = True
+
+if not args.username:
+    args.username = raw_input('Username: ')
+
+if not args.password:
+    args.password = getpass.getpass()
 
 driver = webdriver.PhantomJS()
 driver.set_window_size(800, 600)
 
 driver.get('https://nettskjema.uio.no/user/index.html')
-form = driver.find_element_by_css_selector('#login-box-form')
 
 userfield = driver.find_element_by_css_selector('#username')
-username = raw_input('Username: ')
-userfield.send_keys(username)
+userfield.send_keys(args.username)
 
 passfield = driver.find_element_by_css_selector('#password')
-password = getpass.getpass()
-passfield.send_keys(password)
+passfield.send_keys(args.password)
 
 button = driver.find_element_by_css_selector('#login-box-form .submit')
 button.click()
@@ -40,19 +58,38 @@ forms = driver.find_elements_by_css_selector('.forms .formName')
 
 formdata = map(lambda form: (form.text, form.get_attribute('href')), forms)
 
-# Need to filter here if downloading all is not desired
+if args.filter:
+    filtered = filter(lambda x: args.filter in x[0], formdata)
+    print 'Filter matched {} of {} forms'.format(len(filtered), len(formdata))
+    formdata = filtered
 
 session = requests.Session()
 cookies = driver.get_cookies()
 for cookie in cookies:
     session.cookies.set(cookie['name'], cookie['value'])
 
-if not os.path.exists('tsv'):
-    os.makedirs('tsv')
+tsv_path = os.path.join(args.out, 'tsv')
+html_path = os.path.join(args.out, 'html')
+
+if args.tsv and not os.path.exists(tsv_path):
+    os.makedirs(tsv_path)
+
+if args.html and not os.path.exists(html_path):
+    os.makedirs(html_path)
 
 for (name, url) in formdata:
-    download_url = url.replace('preview', 'download') + '&encoding=utf-8'
-    response = session.get(download_url)
-    filename = 'tsv/' + name + '.tsv'
-    with open(filename, 'w') as f:
-        f.write(response.content)
+    print 'Fetching ' + name
+
+    if args.tsv:
+        tsv_url = url.replace('preview', 'download') + '&encoding=utf-8'
+        response = session.get(tsv_url)
+        filename = os.path.join(tsv_path, name) + '.tsv'
+        with open(filename, 'w') as f:
+            f.write(response.content)
+
+    if args.html:
+        html_url = url.replace('preview', 'report/web') + '&include-open=1&remove-profile=1'
+        response = session.get(html_url)
+        filename = os.path.join(html_path, name) + '.html'
+        with open(filename, 'w') as f:
+            f.write(response.content)
