1439066af841b9be7aba1f824577adc6bed2eeac
Erik Vesteraas <erik@vestera.as>
2016-06-06 13:01:42 +0200

    Refactor download-reports to ensure closing driver

--- scripts/download-reports.py
+++ scripts/download-reports.py
@@ -12,84 +12,103 @@
 # PhantomJS is the engine we use for selenium.
 
 import getpass
-import requests
 import os
 import sys
 import argparse
 
+import requests
+
 from selenium import webdriver
 
-argparser = argparse.ArgumentParser(description='Download report data from nettskjema.uio.no')
-argparser.add_argument('--out', '-o', help='Output directory (default=".")', type=str, default='.')
-argparser.add_argument('--filter', '-f', help='String to filter by', type=str)
-argparser.add_argument('--username', '-u', help='Username for login', type=str)
-argparser.add_argument('--password', '-p', help='Password for login', type=str)
-argparser.add_argument('--tsv', help='Download TSV files', action="store_true")
-argparser.add_argument('--html', help='Download HTML reports', action="store_true")
-args = argparser.parse_args()
+def get_args():
+    argparser = argparse.ArgumentParser(description='Download report data from nettskjema.uio.no')
+    argparser.add_argument('--out', '-o', help='Output directory (default=".")', type=str, default='.')
+    argparser.add_argument('--filter', '-f', help='String to filter by', type=str)
+    argparser.add_argument('--username', '-u', help='Username for login', type=str)
+    argparser.add_argument('--password', '-p', help='Password for login', type=str)
+    argparser.add_argument('--tsv', help='Download TSV files', action="store_true")
+    argparser.add_argument('--html', help='Download HTML reports', action="store_true")
+    args = argparser.parse_args()
 
-if not args.tsv and not args.html:
-    args.tsv = True
-    args.html = True
+    if not args.tsv and not args.html:
+        args.tsv = True
+        args.html = True
 
-if not args.username:
-    args.username = raw_input('Username: ')
+    if not args.username:
+        args.username = raw_input('Username: ')
 
-if not args.password:
-    args.password = getpass.getpass()
+    if not args.password:
+        args.password = getpass.getpass()
 
-driver = webdriver.PhantomJS()
-driver.set_window_size(800, 600)
+    return args
 
-driver.get('https://nettskjema.uio.no/user/index.html')
+def login(driver, args):
+    driver.get('https://nettskjema.uio.no/user/index.html')
 
-userfield = driver.find_element_by_css_selector('#username')
-userfield.send_keys(args.username)
+    userfield = driver.find_element_by_css_selector('#username')
+    userfield.send_keys(args.username)
 
-passfield = driver.find_element_by_css_selector('#password')
-passfield.send_keys(args.password)
+    passfield = driver.find_element_by_css_selector('#password')
+    passfield.send_keys(args.password)
 
-button = driver.find_element_by_css_selector('#login-box-form .submit')
-button.click()
+    button = driver.find_element_by_css_selector('#login-box-form .submit')
+    button.click()
 
-driver.get('https://nettskjema.uio.no/user/form/list.html')
+def download_files(driver, args):
+    driver.get('https://nettskjema.uio.no/user/form/list.html')
 
-forms = driver.find_elements_by_css_selector('.forms .formName')
+    forms = driver.find_elements_by_css_selector('.forms .formName')
 
-formdata = map(lambda form: (form.text, form.get_attribute('href')), forms)
+    formdata = map(lambda form: (form.text, form.get_attribute('href')), forms)
 
-if args.filter:
-    filtered = filter(lambda x: args.filter in x[0], formdata)
-    print 'Filter matched {} of {} forms'.format(len(filtered), len(formdata))
-    formdata = filtered
+    if args.filter:
+        filtered = filter(lambda x: args.filter in x[0], formdata)
+        print 'Filter matched {} of {} forms'.format(len(filtered), len(formdata))
+        formdata = filtered
 
-session = requests.Session()
-cookies = driver.get_cookies()
-for cookie in cookies:
-    session.cookies.set(cookie['name'], cookie['value'])
+    session = requests.Session()
+    cookies = driver.get_cookies()
+    for cookie in cookies:
+        session.cookies.set(cookie['name'], cookie['value'])
 
-tsv_path = os.path.join(args.out, 'tsv')
-html_path = os.path.join(args.out, 'html')
+    tsv_path = os.path.join(args.out, 'tsv')
+    html_path = os.path.join(args.out, 'html')
 
-if args.tsv and not os.path.exists(tsv_path):
-    os.makedirs(tsv_path)
+    if args.tsv and not os.path.exists(tsv_path):
+        os.makedirs(tsv_path)
 
-if args.html and not os.path.exists(html_path):
-    os.makedirs(html_path)
+    if args.html and not os.path.exists(html_path):
+        os.makedirs(html_path)
 
-for (name, url) in formdata:
-    print 'Fetching ' + name
+    for (name, url) in formdata:
+        print 'Fetching ' + name
 
-    if args.tsv:
-        tsv_url = url.replace('preview', 'download') + '&encoding=utf-8'
-        response = session.get(tsv_url)
-        filename = os.path.join(tsv_path, name) + '.tsv'
-        with open(filename, 'w') as f:
-            f.write(response.content)
+        if args.tsv:
+            tsv_url = url.replace('preview', 'download') + '&encoding=utf-8'
+            response = session.get(tsv_url)
+            filename = os.path.join(tsv_path, name) + '.tsv'
+            with open(filename, 'w') as f:
+                f.write(response.content)
 
-    if args.html:
-        html_url = url.replace('preview', 'report/web') + '&include-open=1&remove-profile=1'
-        response = session.get(html_url)
-        filename = os.path.join(html_path, name) + '.html'
-        with open(filename, 'w') as f:
-            f.write(response.content)
+        if args.html:
+            html_url = url.replace('preview', 'report/web') + '&include-open=1&remove-profile=1'
+            response = session.get(html_url)
+            filename = os.path.join(html_path, name) + '.html'
+            with open(filename, 'w') as f:
+                f.write(response.content)
+
+def main():
+    args = get_args()
+
+    driver = webdriver.PhantomJS()
+    driver.set_window_size(800, 600)
+
+    try:
+        login(driver, args)
+        download_files(driver, args)
+    finally:
+        driver.close()
+        driver.quit()
+
+if __name__ == '__main__':
+    main()
