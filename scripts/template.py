cd005d704269b4c395c2e3cf25bb317da3961e16
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2016-06-14 16:55:47 +0200

    (Voluntary) template file for python, first version.

---
+++ scripts/template.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Documentation string"""
+
+__authors__    = ["Person1", "Person2"]
+__email__      = "person@mail"
+__copyright__  = "FUI - Fagutvalget ved Institutt for Informatikk"
+__credits__    = ["Person1", "Person2", "Person3"]
+
+__version__    = "0.1"
+__license__    = "MIT"
+
+import os
+import sys
+from bs4 import BeautifulSoup
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
