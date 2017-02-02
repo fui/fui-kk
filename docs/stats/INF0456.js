4f4b988288dc0db51d858f8a21958ef49c74e1ed
Erik Vesteraas <erik@vestera.as>
2017-02-02 20:50:36 +0100

    Add output of `make web-preview`, for github pages

---
+++ docs/stats/INF0456.js
@@ -0,0 +1,120 @@
+{
+  "course": {
+    "code": "INF0456",
+    "name": "Unknown",
+    "semester": "V2016"
+  },
+  "respondents": {
+    "started": 0,
+    "answered": 3,
+    "invited": 6
+  },
+  "answer_percentage": 50.0,
+  "language": "EN",
+  "questions": {
+    "How do you rate the course in general?": {
+      "counts": {
+        "Exceptionally good": 1,
+        "Very good": 1,
+        "OK": 1
+      },
+      "average": 3.6666666666666665,
+      "average_text": "Very good"
+    },
+    "How do you rate the level of the course?": {
+      "counts": {
+        "Too easy": 1,
+        "Difficult": 1,
+        "Easy": 1
+      },
+      "average": 1.3333333333333333,
+      "average_text": "Easy"
+    },
+    "How do you rate the work load in proportion to the number of credits achieved?": {
+      "counts": {
+        "Too large": 1,
+        "Too small": 1,
+        "OK": 1
+      },
+      "average": 2.0,
+      "average_text": "OK"
+    },
+    "The quality of the lectures?": {
+      "counts": {
+        "Exceptionally good": 1,
+        "No opinion": 1,
+        "Very good": 1
+      },
+      "average": 4.5,
+      "average_text": "Very good"
+    },
+    "The lectures' relevance and usefulness in regard to relaying what you need to learn?": {
+      "counts": {
+        "Not good": 1,
+        "No opinion": 1,
+        "OK": 1
+      },
+      "average": 1.0,
+      "average_text": "Not that good"
+    },
+    "The quality of the group tuition as a whole?": {
+      "counts": {
+        "Exceptionally good": 1,
+        "No opinion": 1,
+        "Not that good": 1
+      },
+      "average": 3.0,
+      "average_text": "Good"
+    },
+    "In what degree does group tuition and weekly assignments give you practical skills and the possibility to make use of the theory at hand?": {
+      "counts": {
+        "No opinion": 2,
+        "Good": 1
+      },
+      "average": 3.0,
+      "average_text": "Good"
+    },
+    "Are the curriculum and learning outcomes of the course sufficiently defined?": {
+      "counts": {
+        "No opinion": 2,
+        "Good": 1
+      },
+      "average": 3.0,
+      "average_text": "Good"
+    },
+    "How do you rate the course material used (text book etc.)?": {
+      "counts": {
+        "Not good": 1,
+        "No opinion": 1,
+        "OK": 1
+      },
+      "average": 1.0,
+      "average_text": "Not that good"
+    },
+    "How do you rate the difficulty level of the obligatory assignments?": {
+      "counts": {
+        "Too high": 1,
+        "Too low": 1,
+        "Low": 1
+      },
+      "average": 1.6666666666666667,
+      "average_text": "OK"
+    },
+    "What is your opinion in regard to the number of obligatory assignments?": {
+      "counts": {
+        "Too high": 1,
+        "OK": 2
+      },
+      "average": 2.6666666666666665,
+      "average_text": "High"
+    },
+    "How do you rate the difficulty level of the final examination?": {
+      "counts": {
+        "Too high": 2,
+        "Not relevant": 1
+      },
+      "average": 4.0,
+      "average_text": "Too high"
+    }
+  }
+}
