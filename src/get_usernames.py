568f913072b256b0a4db9dad87793ddd71f77a7b
Erik Vesteraas <erik@vestera.as>
2017-05-14 11:11:29 +0200

    usernames: Only allow current or previous semester

--- src/get_usernames.py
+++ src/get_usernames.py
@@ -3,13 +3,14 @@ import getpass
 import argparse
 import sys
 import os
+from datetime import date, timedelta
 from file_funcs import load_json
 
 def get_args():
     argparser = argparse.ArgumentParser(description='Get usernames of students taking courses this semester')
     argparser.add_argument('--username', '-u', help='Username for login', type=str)
     argparser.add_argument('--password', '-p', help='Password for login', type=str)
-    argparser.add_argument('--semester', '-s', help='Semester to read data for', type=str)
+    argparser.add_argument('--prev', help='Read data for the previous semester', action='store_true')
     args = argparser.parse_args()
 
     if not args.username:
@@ -18,7 +19,18 @@ def get_args():
     if not args.password:
         args.password = getpass.getpass()
 
+    if args.prev:
+        args.semester = semester_string(date.today() - timedelta(days=182))
+    else:
+        args.semester = semester_string(date.today())
+
     return args
+
+def semester_string(today):
+    if today.month <= 7:
+        return 'V' + str(today.year)
+    else:
+        return 'H' + str(today.year)
 
 def coursename_to_lsng_arg(coursename):
     return 's' + coursename.lower().replace('-', '')
